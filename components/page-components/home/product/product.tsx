ea08ea780c970619b721225cd88e8c26abcc95bf
Josh <joshua.foster@earthware.co.uk>
2020-12-12 10:55:33 +0000

    Home page - products row

--- components/page-components/home/product/product.tsx
+++ components/page-components/home/product/product.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import styles from './product.module.scss';
+import { Card } from 'react-bootstrap';
 
 type Props = {
   image: string;
@@ -9,11 +9,34 @@ type Props = {
 
 export default function Product(props: Props) {
   return (
-    <div className={styles.productContainer}>
-      <Image src={props.image} alt={props.text} height={320} width={240} layout="fixed" />
-      {props.text}
-      <hr />
-      {props.cost}
-    </div>
+    <Card style={styles.card}>
+      <Image src={props.image} alt={props.text} height={480} width={360} layout="responsive" />
+      <Card.Title style={styles.title}>{props.text}</Card.Title>
+      <hr style={styles.hr}/>
+      <Card.Text style={styles.text}>{props.cost}</Card.Text>
+    </Card>
   );
 }
+
+const styles = {
+  card: {
+    textAlign: 'center',
+    border: 'none',
+    margin: '10px',
+    width: '300px'
+  } as React.CSSProperties,
+  title: {
+    fontSize: 32,
+    margin: '20px 0'
+  } as React.CSSProperties,
+  hr: {
+    width: '35%',
+    margin: 'auto',
+    borderTop: '3px solid rgba(0, 0, 0, 0.1)'
+  } as React.CSSProperties,
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    margin: '20px 0'
+  } as React.CSSProperties
+};
--- components/page-components/home/products/products.tsx
+++ components/page-components/home/products/products.tsx
@@ -1,10 +1,38 @@
+import Link from "next/link";
 import { Container } from "react-bootstrap";
 import Product from '../product/product';
 
 export default function Products() {
   return (
-    <Container>
-      <Product image={'/cakes/saltedcaramel.png'} text="Salted Caramel Brownie" cost="£4.00" />
-    </Container>
+    <>
+      <Container style={styles.productsContainer}>
+        <Product image={'/cakes/saltedcaramel.png'} text="Salted Caramel Brownie" cost="£4.00" />
+        <Product image={'/cakes/chocolateguinness.png'} text="Chocolate Guiness Cake" cost="£4.00" />
+        <Product image={'/cakes/brownbuttersponge.png'} text="Brown Butter Spongecake" cost="£4.00" />
+        <Product image={'/cakes/cinnamonbun.png'} text="Cinnamon Bun" cost="£4.00" />
+        <Product image={'/cakes/lemonmeringue.png'} text="Lemon Meringue" cost="£4.00" />
+        <Product image={'/cakes/vanillasponge.png'} text="Vanilla Spongecake" cost="£4.00" />
+      </Container>
+
+      <div style={styles.buttonContainer}>
+        <Link href="/">
+          <a>More...</a>
+        </Link>
+      </div>
+    </>
   );
 }
+
+const styles = {
+  productsContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  } as React.CSSProperties,
+  buttonContainer: {
+    width: '100%',
+    textAlign: 'center',
+    fontSize: '32px',
+    padding: "20px 0"
+  } as React.CSSProperties
+};
