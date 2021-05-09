02bea45cf0b6e2ea21f1985bd4235ad51ba48fcd
Josh <joshua.foster@earthware.co.uk>
2021-05-09 11:58:16 +0100

    Text amendment

--- components/page-components/celebration-cakes/AllergyHelp.tsx
+++ components/page-components/celebration-cakes/AllergyHelp.tsx
@@ -13,7 +13,7 @@ export const AllergyHelp = () => (
   <Wrapper>
     <Container>
       <SubTitle>Allergies and dietary requirements</SubTitle>
-      <Paragraph>Please disclose any allergies or dietary requirements when ordering or email to enquire. Due to allergens (e.g. nuts, gluten) being used in the same area, I cannot say products are completely free from certain ingredients but can specify what a product is made without.</Paragraph>
+      <Paragraph>Please disclose any allergies or dietary requirements when ordering or email to enquire. Due to allergens (e.g. nuts, gluten) being used in the same area, I cannot say products are completely free from certain ingredients but can specify which products are made without.</Paragraph>
     </Container>
   </Wrapper>
 );
